4ce8c061ba6f2ee9726c134ceef3cf3b29052f47
matsui54 <haru.matu9168@gmail.com>
2022-03-09 13:32:18 +0900

    Automate upgrading deps

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ddu-source-command_history
+
 Command line history source for ddu.vim
--- denops/@ddu-kinds/command_history.ts
+++ denops/@ddu-kinds/command_history.ts
@@ -1,4 +1,4 @@
-import { BaseKind } from "https://deno.land/x/ddu_vim@v0.12.2/types.ts#^";
+import { BaseKind } from "https://deno.land/x/ddu_vim@v1.2.0/types.ts";
 
 type Params = Record<never, never>;
 
