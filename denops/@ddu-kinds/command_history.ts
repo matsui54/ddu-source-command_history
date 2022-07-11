00ddc2cf7ff5d546f21bebd8385fd696d4bd2c00
matsui54 <matsui54@users.noreply.github.com>
2022-07-11 00:43:45 +0000

    :package: Update Deno dependencies
    Update dependencies by udd:
    
    /home/runner/work/ddu-source-command_history/ddu-source-command_history/denops/@ddu-sources/command_history.ts
    [1/2] Looking for releases: https://deno.land/x/ddu_vim@v1.8.4/types.ts
    [1/2] Attempting update: https://deno.land/x/ddu_vim@v1.8.4/types.ts -> v1.8.7
    [1/2] Update successful: https://deno.land/x/ddu_vim@v1.8.4/types.ts -> v1.8.7
    [2/2] Looking for releases: https://deno.land/x/ddu_vim@v1.8.4/deps.ts
    [2/2] Attempting update: https://deno.land/x/ddu_vim@v1.8.4/deps.ts -> v1.8.7
    [2/2] Update successful: https://deno.land/x/ddu_vim@v1.8.4/deps.ts -> v1.8.7
    
    /home/runner/work/ddu-source-command_history/ddu-source-command_history/denops/@ddu-kinds/command_history.ts
    [1/1] Looking for releases: https://deno.land/x/ddu_vim@v1.8.4/types.ts
    [1/1] Attempting update: https://deno.land/x/ddu_vim@v1.8.4/types.ts -> v1.8.7
    [1/1] Update successful: https://deno.land/x/ddu_vim@v1.8.4/types.ts -> v1.8.7
    
    /home/runner/work/ddu-source-command_history/ddu-source-command_history/README.md
    
    Successfully updated:
    https://deno.land/x/ddu_vim@v1.8.4/types.ts v1.8.4 -> v1.8.7
    https://deno.land/x/ddu_vim@v1.8.4/deps.ts v1.8.4 -> v1.8.7
    https://deno.land/x/ddu_vim@v1.8.4/types.ts v1.8.4 -> v1.8.7
    make[1]: Entering directory '/home/runner/work/ddu-source-command_history/ddu-source-command_history'
    make[1]: Leaving directory '/home/runner/work/ddu-source-command_history/ddu-source-command_history'

--- denops/@ddu-kinds/command_history.ts
+++ denops/@ddu-kinds/command_history.ts
@@ -1,4 +1,4 @@
-import { BaseKind } from "https://deno.land/x/ddu_vim@v1.8.4/types.ts";
+import { BaseKind } from "https://deno.land/x/ddu_vim@v1.8.7/types.ts";
 
 type Params = Record<never, never>;
 
