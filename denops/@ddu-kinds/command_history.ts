342ebbbf38bcf3f9ba956f90d6800d0228d7904a
matsui54 <haru.matu9168@gmail.com>
2023-01-07 16:53:33 +0900

    Remove github workflow

--- denops/@ddu-kinds/command_history.ts
+++ denops/@ddu-kinds/command_history.ts
@@ -1,4 +1,4 @@
-import { BaseKind } from "https://deno.land/x/ddu_vim@v2.0.0/types.ts";
+import { BaseKind } from "https://deno.land/x/ddu_vim@v2.1.0/types.ts";
 
 type Params = Record<never, never>;
 
