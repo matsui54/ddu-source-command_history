63840ec2d0e5678881f3473bb4649219b11a4232
matsui54 <haru.matu9168@gmail.com>
2023-05-16 19:39:46 +0900

    📦 Update Deno dependencies

--- denops/@ddu-sources/command_history.ts
+++ denops/@ddu-sources/command_history.ts
@@ -3,13 +3,13 @@ import {
   ActionFlags,
   BaseSource,
   Item,
-} from "https://deno.land/x/ddu_vim@v2.7.0/types.ts";
+} from "https://deno.land/x/ddu_vim@v2.8.4/types.ts";
 import {
   batch,
   Denops,
   fn,
   gather,
-} from "https://deno.land/x/ddu_vim@v2.7.0/deps.ts";
+} from "https://deno.land/x/ddu_vim@v2.8.4/deps.ts";
 
 export type ActionData = {
   command: string;
